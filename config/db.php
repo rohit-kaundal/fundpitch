a796a020e0865c0029d0f42c1e1c0ccc7f294fd2
Rohit Kaundal <kaundal.rohit@gmail.com>
2016-04-13 12:04:55 +0000

    added db config

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=fundpitch_db',
     'username' => 'root',
-    'password' => '',
+    'password' => '3zAWPqgSp5',
     'charset' => 'utf8',
 ];
